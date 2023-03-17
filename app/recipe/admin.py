2c0c15eda908bcb121960e0cd46cede2d416a9a6
amoldhoke <amoldhoke07@gmail.com>
2023-03-17 21:29:33 +0530

    API Documentation and drf_spectacular(SwaggerUI)

--- app/recipe/admin.py
+++ app/recipe/admin.py
@@ -1,11 +1,12 @@
 """
-Django admin customization.  
+Django admin customization.
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
 from recipe import models
+
 
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
@@ -40,5 +41,6 @@ class UserAdmin(BaseUserAdmin):
             )
         }),
     )
+
 
 admin.site.register(models.User, UserAdmin)
--- app/recipe/models.py
+++ app/recipe/models.py
@@ -9,6 +9,7 @@ from django.contrib.auth.models import (
     PermissionsMixin
 )
 
+
 class UserManager(BaseUserManager):
     """Manager for users."""
 
@@ -21,7 +22,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
-    
+
     def create_superuser(self, email, password):
         """Create and return a new superuser."""
         user = self.create_user(email, password)
@@ -30,6 +31,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
